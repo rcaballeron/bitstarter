2fc35b8fe2ae1552c88e960f295902aa1082c952
Ubuntu <ubuntu@ip-172-31-26-17.us-west-2.compute.internal>
2013-07-10 12:49:54 +0000

    Fixed encoding

--- web.js
+++ web.js
@@ -4,9 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	
-		buf = new Buffer(fs.readFileSync('index.html', 'utf8'));
-		response.send(buf.toString());
+	buf = new Buffer(fs.readFileSync('index.html'));
+	response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
